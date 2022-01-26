17c4d1bd8abf9093fb3020c3503541c6e46e1f5a
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2022-01-27 01:26:25 +0530

    temporary deployment

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Which of the following can cause dry mouth as a side effect?",
+				option1: "Guaifenesin",
+				option2: "Dextromethorphan",
+				option3: "Diphenhydramine",
+				option4: "Simple linctus",
+				answer: 3
+			},
+			{
+				question: "Contractions of the respiratory muscles result in a(n):",
+				option1: "Decrease in the volume of the thorax",
+				option2: "Increase in the volume of the thorax",
+				option3: "Increase in the amount of blood flowing through the lungs",
+				option4: "Decrease in the amount of blood flowing through the lungs",
+				answer: 2
+			},
+			{
+				question: "The type of emphysema associated with smoking is",
+				option1: "Panacinar",
+				option2: "Centriacinar",
+				option3: "Distal acinar",
+				option4: "Irregular",
+				answer: 2
+			},
+			{
+				question: "With regards to obstructive sleep apnoea (OSA), which of the following statements is INCORRECT?",
+				option1: "Hhypoxaemia is the main stimulus to arousal",
+				option2: "Main method of treating this syndrome is with Continuous Positive Airway Pressure (CPAP)",
+				option3: "This syndrome occurs in up to 5% of adults",
+				option4: "This syndrome rarely has an obstructive component",
+				answer: 4
+			},
+			{
+				question: "Which bronchial secretion enhancer is approved by U.S FDA?",
+				option1: "Guaiphenesin",
+				option2: "Potassium iodide",
+				option3: "Ammonium chloride",
+				option4: "Potassium citrate",
+				answer: 1
+			}
+		]
+	];
